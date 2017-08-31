3b8149ab5ce367ec01b33f86a21631fc6f5fc678
Tom Clarkson <tom@designsight.co.nz>
2017-08-31 13:17:13 +1200

    build with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,17 @@ module.exports = {
     entry: "./src/index.js",
     output: {
         path: __dirname,
-        filename: "bundle.js"
+        filename: "bundle.js",
+        libraryTarget: 'umd',
+        library: 'aui'
     },
     module: {
         loaders: [
             { test: /\.js$/, loader: "babel-loader",exclude: /node_modules/ },
             { test: /\.css$/, loader: "style-loader!css-loader" }
         ]
+    },
+    externals: {
+        react: 'react'
     }
 };
